b437af70f19c17402db6b60758e3d342b90cac26
NatelleZiegler <nkz5@pitt.edu>
2017-02-06 21:38:31 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,8 +1,8 @@
 public class Person {
 	public static void main(String[] args) {
 		System.out.print("Natelle Ziegler");
-		System.out.print("\n628 Hamilton Road");
-		System.out.print("\nPittsburgh PA 15205");
+		System.out.print("\n777 Puddleby Ave");
+		System.out.print("\nPittsburgh PA 11111");
 		System.out.print("\n412-780-3828\n");
 	}
 }
