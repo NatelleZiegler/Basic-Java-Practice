65ae078ae116453b2a715de6c3b1436719a96710
NatelleZiegler <nkz5@pitt.edu>
2017-02-06 21:40:04 -0500

    Update PersonalInfo.java

--- PersonalInfo.java
+++ PersonalInfo.java
@@ -1,8 +1,8 @@
 public class PersonalInfo {
 	public static void main(String[] args) {
 		String name = "Natelle Ziegler";
-		String address = "628 Hamilton Road Pittsburgh, PA 15205";
-		String telephone = "4129228457";
+		String address = "77 Puddleby Ave Pittsburgh, PA 11111";
+		String telephone = "4217803828";
 		String major = "Film";
 		
 		System.out.println(name + "\n" + address + "\n" + telephone + "\n" + major + "\n");
